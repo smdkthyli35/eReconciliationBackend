612fb618f840cac1a342cedab2697f7c767a709c
zeljkosamed <smdkthyli@gmail.com>
2022-04-17 17:25:42 +0300

    EfEntityRepositoryBase added.

---
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,53 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess.EntityFramework
+{
+    public class EfEntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity>
+        where TEntity : class, IEntity, new()
+        where TContext : DbContext, new()
+    {
+        protected TContext _context { get; }
+
+        public EfEntityRepositoryBase(TContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(TEntity entity)
+        {
+            _context.Entry(entity).State = EntityState.Added;
+            _context.SaveChanges();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _context.Entry(entity).State = EntityState.Deleted;
+            _context.SaveChanges();
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            return _context.Set<TEntity>().SingleOrDefault(filter);
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            return filter == null
+                ? _context.Set<TEntity>().ToList()
+                : _context.Set<TEntity>().Where(filter).ToList();
+        }
+
+        public void Update(TEntity entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+    }
+}
