0a0f41de064c10295d65c8b68643614c388548a6
zeljkosamed <smdkthyli@gmail.com>
2022-04-17 17:06:39 +0300

    Database connection added.

---
+++ DataAccess/Concrete/EntityFramework/Contexts/BaseDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework.Contexts
+{
+    public class BaseDbContext : DbContext
+    {
+        public BaseDbContext(DbContextOptions<BaseDbContext> options) : base(options)
+        {
+
+        }
+
+        public BaseDbContext()
+        {
+
+        }
+
+        public DbSet<AccountReconciliation> AccountReconciliations { get; set; }
+        public DbSet<AccountReconciliationDetail> AccountReconciliationDetails { get; set; }
+        public DbSet<BaBsReconciliation> BaBsReconciliations { get; set; }
+        public DbSet<BaBsReconciliationDetail> BaBsReconciliationDetails { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Currency> Currencies { get; set; }
+        public DbSet<CurrencyAccount> CurrencyAccounts { get; set; }
+        public DbSet<MailParameter> MailParameters { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+        public DbSet<UserCompany> UserCompanies { get; set; }
+    }
+}
