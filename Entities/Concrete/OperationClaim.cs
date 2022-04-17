66de74a7d986d64832c2e27528c25b52c311a037
zeljkosamed <smdkthyli@gmail.com>
2022-04-17 16:31:32 +0300

    Refactor entities.

--- Entities/Concrete/OperationClaim.cs
+++ Entities/Concrete/OperationClaim.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Core.Entities.Concrete
+namespace Entities.Concrete
 {
     public class OperationClaim : IEntity
     {
--- Entities/Concrete/UserOperationClaim.cs
+++ Entities/Concrete/UserOperationClaim.cs
@@ -1,16 +1,17 @@
-﻿using System;
+﻿using Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Core.Entities.Concrete
+namespace Entities.Concrete
 {
-    public class UserCompany : IEntity
+    public class UserOperationClaim : IEntity
     {
         public int Id { get; set; }
         public int UserId { get; set; }
-        public int CompanyId { get; set; }
+        public int OperationClaimId { get; set; }
         public DateTime AddedAt { get; set; }
         public bool IsActive { get; set; }
     }
