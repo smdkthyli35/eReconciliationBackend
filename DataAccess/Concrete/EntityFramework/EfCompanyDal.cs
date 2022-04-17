359799aed138e12c6c1725f9e8b20eae9c516907
zeljkosamed <smdkthyli@gmail.com>
2022-04-17 17:36:07 +0300

    Generic repository implemented to interfaces.

---
+++ DataAccess/Concrete/EntityFramework/EfCompanyDal.cs
@@ -0,0 +1,19 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework.Contexts;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCompanyDal : EfEntityRepositoryBase<Company, BaseDbContext>, ICompanyDal
+    {
+        public EfCompanyDal(BaseDbContext context) : base(context)
+        {
+        }
+    }
+}
