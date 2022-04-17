e389672cfa0780c0b4297874dbc6bcea7a79d6f4
zeljkosamed <smdkthyli@gmail.com>
2022-04-17 17:54:19 +0300

    Generic repository implemented to classes.

---
+++ DataAccess/Concrete/EntityFramework/EfBaBsReconciliationDetailDal.cs
@@ -0,0 +1,19 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework.Contexts;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfBaBsReconciliationDetailDal : EfEntityRepositoryBase<BaBsReconciliationDetail, BaseDbContext>, IBaBsReconciliationDetailDal
+    {
+        public EfBaBsReconciliationDetailDal(BaseDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/EfUserDal.cs
@@ -0,0 +1,19 @@
+﻿using Core.DataAccess.EntityFramework;
+using Core.Entities.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfUserDal : EfEntityRepositoryBase<User, BaseDbContext>, IUserDal
+    {
+        public EfUserDal(BaseDbContext context) : base(context)
+        {
+        }
+    }
+}
