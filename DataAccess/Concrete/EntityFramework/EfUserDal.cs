9dfe1b17e8cfca29d982986a723fe836e9ff9412
zeljkosamed <smdkthyli@gmail.com>
2022-04-18 18:13:06 +0300

    UserForLoginDto, UserForRegisterDto and IUserDal GetClaim method added.

--- DataAccess/Concrete/EntityFramework/EfUserDal.cs
+++ DataAccess/Concrete/EntityFramework/EfUserDal.cs
@@ -15,5 +15,22 @@ namespace DataAccess.Concrete.EntityFramework
         public EfUserDal(BaseDbContext context) : base(context)
         {
         }
+
+        public List<OperationClaim> GetClaims(User user, int companyId)
+        {
+            var result = from operationClaim in BaseDbContext.OperationClaims
+                         join userOperationClaim in BaseDbContext.UserOperationClaims
+                         on operationClaim.Id equals userOperationClaim.OperationClaimId
+                         where userOperationClaim.CompanyId == companyId && userOperationClaim.UserId == user.Id
+                         select new OperationClaim
+                         {
+                             Id = operationClaim.Id,
+                             Name = operationClaim.Name
+                         };
+
+            return result.ToList();
+        }
+
+        public BaseDbContext BaseDbContext => _context as BaseDbContext;
     }
 }
