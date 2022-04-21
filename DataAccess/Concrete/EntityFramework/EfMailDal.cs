27f10ffcf80e1dc915d38236878911877c458c7e
zeljkosamed <smdkthyli@gmail.com>
2022-04-21 15:29:56 +0300

    Generic mail structure added.

---
+++ DataAccess/Concrete/EntityFramework/EfMailDal.cs
@@ -0,0 +1,37 @@
+﻿using DataAccess.Abstract;
+using Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfMailDal : IMailDal
+    {
+        public void SendMail(SendMailDto sendMailDto)
+        {
+            using (MailMessage mail = new MailMessage())
+            {
+                mail.From = new MailAddress(sendMailDto.MailParameter.Email);
+                mail.To.Add(sendMailDto.Email);
+                mail.Subject = sendMailDto.Subject;
+                mail.Body = sendMailDto.Body;
+                mail.IsBodyHtml = true;
+                //mail.Attachments.Add();
+
+                using (SmtpClient smtp = new SmtpClient(sendMailDto.MailParameter.SMTP))
+                {
+                    smtp.UseDefaultCredentials = false;
+                    smtp.Credentials = new NetworkCredential(sendMailDto.MailParameter.Email, sendMailDto.MailParameter.Password);
+                    smtp.EnableSsl = sendMailDto.MailParameter.SSL;
+                    smtp.Send(mail);
+                }
+
+            }
+        }
+    }
+}
---
+++ Entities/Dtos/SendMailDto.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Dtos
+{
+    public class SendMailDto
+    {
+        public MailParameter MailParameter { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+
+    }
+}
