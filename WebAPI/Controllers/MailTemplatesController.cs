e6d026451195d2c02d297c59851267828500afaf
zeljkosamed <smdkthyli@gmail.com>
2022-04-22 23:50:05 +0300

    MailTemplateController add method added.

---
+++ WebAPI/Controllers/MailTemplatesController.cs
@@ -0,0 +1,32 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MailTemplatesController : ControllerBase
+    {
+        private readonly IMailTemplateService _mailTemplateService;
+
+        public MailTemplatesController(IMailTemplateService mailTemplateService)
+        {
+            _mailTemplateService = mailTemplateService;
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(MailTemplate mailTemplate)
+        {
+            var result = _mailTemplateService.Add(mailTemplate);
+            if (result.Success)
+                return Ok(result);
+            return BadRequest(result.Message);
+        }
+    }
+}
