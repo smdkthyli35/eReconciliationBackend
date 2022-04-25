8bec0237c5d9f53655e1af2f6657280eaa0dea81
zeljkosamed <smdkthyli@gmail.com>
2022-04-25 23:17:11 +0300

    Added Autofac dynamic proxy configuration to business layer.

--- Business/Concrete/UserManager.cs
+++ Business/Concrete/UserManager.cs
@@ -1,4 +1,6 @@
 ﻿using Business.Abstract;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.Entities.Concrete;
 using DataAccess.Abstract;
 using System;
@@ -18,6 +20,7 @@ namespace Business.Concrete
             _userDal = userDal;
         }
 
+        [ValidationAspect(typeof(UserValidator))]
         public void Add(User user)
         {
             _userDal.Add(user);
