39539388bdae376497e89ad49f5f6edbeb86157a
zeljkosamed <smdkthyli@gmail.com>
2022-04-28 16:08:47 +0300

    Currency account controller and get getbyid add update delete methods added.

---
+++ WebAPI/Controllers/CurrencyAccountsController.cs
@@ -0,0 +1,78 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CurrencyAccountsController : ControllerBase
+    {
+        private readonly ICurrencyAccountService _currencyAccountService;
+
+        public CurrencyAccountsController(ICurrencyAccountService currencyAccountService)
+        {
+            _currencyAccountService = currencyAccountService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll(int companyId)
+        {
+            var result = _currencyAccountService.GetAll(companyId);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _currencyAccountService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(CurrencyAccount currencyAccount)
+        {
+            var result = _currencyAccountService.Add(currencyAccount);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("update")]
+        public IActionResult Update(CurrencyAccount currencyAccount)
+        {
+            var result = _currencyAccountService.Update(currencyAccount);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("delete")]
+        public IActionResult Delete(CurrencyAccount currencyAccount)
+        {
+            var result = _currencyAccountService.Delete(currencyAccount);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+    }
+}
