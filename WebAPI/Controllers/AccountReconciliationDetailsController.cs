8e5cbb73f002d6cc9c0f842c626dadbaed20aa1c
zeljkosamed <smdkthyli@gmail.com>
2022-05-02 16:45:44 +0300

    AccountReconciliationDetailsController added.

---
+++ WebAPI/Controllers/AccountReconciliationDetailsController.cs
@@ -0,0 +1,102 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountReconciliationDetailsController : ControllerBase
+    {
+        private readonly IAccountReconciliationDetailService _accountReconciliationDetailService;
+
+        public AccountReconciliationDetailsController(IAccountReconciliationDetailService accountReconciliationDetailService)
+        {
+            _accountReconciliationDetailService = accountReconciliationDetailService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll(int accountReconciliationId)
+        {
+            var result = _accountReconciliationDetailService.GetAll(accountReconciliationId);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _accountReconciliationDetailService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("addfromexcel")]
+        public IActionResult AddFromExcel(IFormFile file, int accountReconciliationId)
+        {
+            if (file.Length > 0)
+            {
+                var fileName = Guid.NewGuid().ToString() + ".xlsx";
+                var filePath = $"{Directory.GetCurrentDirectory()}/Content/{fileName}";
+                using (FileStream stream = System.IO.File.Create(filePath))
+                {
+                    file.CopyTo(stream);
+                    stream.Flush();
+                }
+
+                var result = _accountReconciliationDetailService.AddToExcel(filePath, accountReconciliationId);
+                if (result.Success)
+                {
+                    return Ok(result);
+                }
+                return BadRequest(result.Message);
+            }
+            return BadRequest("Dosya seçimi yapmadınız!");
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(AccountReconciliationDetail accountReconciliationDetail)
+        {
+            var result = _accountReconciliationDetailService.Add(accountReconciliationDetail);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("update")]
+        public IActionResult Update(AccountReconciliationDetail accountReconciliationDetail)
+        {
+            var result = _accountReconciliationDetailService.Update(accountReconciliationDetail);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("delete")]
+        public IActionResult Delete(AccountReconciliationDetail accountReconciliationDetail)
+        {
+            var result = _accountReconciliationDetailService.Delete(accountReconciliationDetail);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+    }
+}
