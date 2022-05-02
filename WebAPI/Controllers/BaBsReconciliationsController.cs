99aaac77fe3006dad8978ed8690f087311d61c5b
zeljkosamed <smdkthyli@gmail.com>
2022-05-02 17:05:49 +0300

    BaBsReconciliationController added.

---
+++ WebAPI/Controllers/BaBsReconciliationsController.cs
@@ -0,0 +1,102 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaBsReconciliationsController : ControllerBase
+    {
+        private readonly IBaBsReconciliationService _baBsReconciliationService;
+
+        public BaBsReconciliationsController(IBaBsReconciliationService baBsReconciliationService)
+        {
+            _baBsReconciliationService = baBsReconciliationService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll(int companyId)
+        {
+            var result = _baBsReconciliationService.GetAll(companyId);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _baBsReconciliationService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("addfromexcel")]
+        public IActionResult AddFromExcel(IFormFile file, int companyId)
+        {
+            if (file.Length > 0)
+            {
+                var fileName = Guid.NewGuid().ToString() + ".xlsx";
+                var filePath = $"{Directory.GetCurrentDirectory()}/Content/{fileName}";
+                using (FileStream stream = System.IO.File.Create(filePath))
+                {
+                    file.CopyTo(stream);
+                    stream.Flush();
+                }
+
+                var result = _baBsReconciliationService.AddToExcel(filePath, companyId);
+                if (result.Success)
+                {
+                    return Ok(result);
+                }
+                return BadRequest(result.Message);
+            }
+            return BadRequest("Dosya seçimi yapmadınız!");
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(BaBsReconciliation baBsReconciliation)
+        {
+            var result = _baBsReconciliationService.Add(baBsReconciliation);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("update")]
+        public IActionResult Update(BaBsReconciliation baBsReconciliation)
+        {
+            var result = _baBsReconciliationService.Update(baBsReconciliation);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("delete")]
+        public IActionResult Delete(BaBsReconciliation baBsReconciliation)
+        {
+            var result = _baBsReconciliationService.Delete(baBsReconciliation);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+    }
+}
